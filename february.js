bbf0754c705d126eea33001e4c0879205ab8062a
trxxxx4 <149764131+trxxxx4@users.noreply.github.com>
2024-02-23 18:14:00 +0500

    Add files via upload

--- february.js
+++ february.js
@@ -1,20 +1,37 @@
-function revealFunction(){
-    window.sr = ScrollReveal({
-        duration: 1250,  reset: true
-    });
-    sr.reveal('header', {origin: 'left', distance: '250px', reset: false});
-    sr.reveal('.title__h2__header', {origin:'right', distance: '350px', delay: 700});
-    sr.reveal('.title__h2__footer', {origin:'left', distance: '300px'});
-    sr.reveal('.citate', {origin: 'bottom', distance:'200px', delay: 200});
-    sr.reveal('.text__title', {origin: 'bottom', distance:'250px'});
-    sr.reveal('.text__p', {origin: 'right', distance:'270px'});
-    sr.reveal('#photo__1', {origin: 'left', distance:'400px'});
-    sr.reveal('#photo__2', {origin: 'right', distance:'420px'});
-    sr.reveal('#photo__3', {origin: 'bottom', distance:'350px'});
-    sr.reveal('#photo__4', {origin: 'bottom', distance:'300px'});
-    sr.reveal('#photo__5', {origin: 'right', distance:'380px'});
-    sr.reveal('#photo__6', {origin: 'top', distance:'400px'});
-    sr.reveal('.footer__sr', {origin: 'top', distance:'200px', duration: 1000, reset: false});
+const artemPage = document.querySelector('[data-page__owner="artem"]');
+const vladPage = document.querySelector('[data-page__owner="vlad"]');
+if (vladPage) {
+
+    function revealFunction() {
+        window.sr = ScrollReveal({
+            duration: 1250, reset: true
+        });
+        sr.reveal('header', { origin: 'left', distance: '250px', reset: false });
+        sr.reveal('.title__h2__header', { origin: 'right', distance: '350px', delay: 700 });
+        sr.reveal('.title__h2__footer', { origin: 'left', distance: '300px' });
+        sr.reveal('.citate', { origin: 'bottom', distance: '200px', delay: 200 });
+        sr.reveal('.text__title', { origin: 'bottom', distance: '250px' });
+        sr.reveal('.text__p', { origin: 'right', distance: '270px' });
+        sr.reveal('#photo__1', { origin: 'left', distance: '400px' });
+        sr.reveal('#photo__2', { origin: 'right', distance: '420px' });
+        sr.reveal('#photo__3', { origin: 'bottom', distance: '350px' });
+        sr.reveal('#photo__4', { origin: 'bottom', distance: '300px' });
+        sr.reveal('#photo__5', { origin: 'right', distance: '380px' });
+        sr.reveal('#photo__6', { origin: 'top', distance: '400px' });
+        sr.reveal('.footer__sr', { origin: 'top', distance: '200px', duration: 1000, reset: false });
+    }
+}
+
+
+
+if (artemPage){
+    function revealFunction() {
+        window.sr = ScrollReveal({
+            duration: 1250, reset: true
+        });
+        sr.reveal('header', { origin: 'top', distance: '550px', reset: false });
+        
+    }
 }
 
 
@@ -25,7 +42,13 @@ function revealFunction(){
 
 
 
-window.addEventListener('load', () =>{
+
+
+
+
+
+
+window.addEventListener('load', () => {
     revealFunction();
     let hidden = document.querySelectorAll(".hidden");
     hidden.forEach(element => {
